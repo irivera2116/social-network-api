ff4a3ee0e458f6ef295e48b3994116d25ec88b97
irivera2116 <irivera2116@GMAIL.COM>
2023-08-14 12:03:30 -0400

    added moment and mongodb to json, worked on thoughts and reactions

---
+++ models/reaction.js
@@ -0,0 +1,35 @@
+// require moment
+const moment = require('moment');
+const { Schema, Types } = require('mongoose');
+
+// build the Schema
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            reuqired: true,
+        },
+        createdAt: {
+            type: Data,
+            default: Date.now,
+            get: (timestamp) => moment(timestamp).format('MMM D, YYYY [at] hh:mm a'),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+module.exports = reactionSchema;
